08a023565070fa487aab636ec7fed262dd5f59e1
appy-hour <81193735+rebecafelicissimo@users.noreply.github.com>
2022-06-13 17:15:03 -0300

    Update site
    Inserimos formulário - falta algumas validações no JS

--- main.js
+++ main.js
@@ -18,3 +18,87 @@ function changeNavColorWhenScrolled() {
 // function closeMenu() {
 //   document.body.classList.remove('menu-expanded')
 // }
+
+//====== Formulário ======
+const form = document.querySelector('#form')
+const userName = document.querySelector('#userName')
+const email = document.querySelector('#email')
+// const password = document.querySelector('#password')
+// const passwordConfirmation = document.querySelector('#passwordConfirmation')
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault() // para não enviar o formulário
+
+  checkInputs()
+})
+
+function checkInputs() {
+  // pegar os valores dos inputs e armazenar em variáveis
+  const userNameValue = userName.value
+  const emailValue = email.value
+  // const passwordValue = password.value
+  // const passwordConfirmationValue = passwordConfirmation.value
+
+  if (userNameValue === '') {
+    setErrorFor(userName, 'O nome é obrigatório.')
+  } else {
+    setSuccessFor(userName)
+  }
+
+  if (emailValue === '') {
+    setErrorFor(email, 'O email é obrigatório.')
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, 'Por favor digite um e-mail válido.')
+  } else {
+    setSuccessFor(email)
+  }
+
+  // if (passwordValue === '') {
+  //   setErrorFor(password, 'A senha é obrigatória.')
+  // } else if (passwordValue.length < 7) {
+  //   setErrorFor(password, 'A senha deve ter no mínimo 7 caracteres.')
+  // } else {
+  //   setSuccessFor(password)
+  // }
+
+  // if (passwordConfirmationValue === '') {
+  //   setErrorFor(passwordConfirmation, 'A confirmação de senha é obrigatória.')
+  // } else if (passwordConfirmationValue !== passwordValue) {
+  //   setErrorFor(passwordConfirmation, 'As senhas não estão iguais.')
+  // } else {
+  //   setSuccessFor(passwordConfirmation)
+  // }
+
+  const formControls = form.querySelectorAll('.form-control')
+  const formIsValid = [...formControls].every(formControl => {
+    return formControl.className === 'form-control success'
+  }) // [... formControls] é um NodeList
+
+  if (formIsValid) {
+    console.log('O formulário está 100% válido!')
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement
+  const small = formControl.querySelector('small')
+
+  //Adicionar a mensagem de erro
+  small.innerText = message
+
+  // Adicionar a classe de erro
+  formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement
+
+  //Adicionar a classe de sucesso
+  formControl.className = 'form-control success'
+}
+
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  )
+}
